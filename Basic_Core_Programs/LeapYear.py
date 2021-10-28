6b1bdf41a1564de056356d1eb67893cb664d5fcd
HemanthGK-GK <hemanthgk804@gmail.com>
2021-10-28 15:20:03 +0530

    Hemanth:Find the year is leap year or not

---
+++ Basic_Core_Programs/LeapYear.py
@@ -0,0 +1,11 @@
+# Program to find the year is Leap year or not
+def leapYear(year):
+    if (((year % 4 == 0) and (year % 100!= 0)) or (year%400 == 0)):
+        print("{0} is a leap year".format(year))
+    else:
+        print("{0} is not a leap year".format(year))
+
+leapYear(2021)
+
+
+                 
