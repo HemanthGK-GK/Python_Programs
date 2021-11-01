dca46a738d49abd1dcf7ee3f08db8a87ef6c9192
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-01 23:45:31 +0530

    [Added]Iterate Dictionary using for loop

---
+++ DataStructure/Dictionary/iterateFor.py
@@ -0,0 +1,4 @@
+# Write a Python program to iterate over dictionaries using for loops.
+dict = {'key1': 'maintain', 'key2': 'silence', 'key3': 'please'}
+for key, value in dict.items():#for loop for dictionary
+    print(key, value)
