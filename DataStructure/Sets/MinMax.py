3406f18999fc62670262af6c3d367d5cf8d8d718
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-02 00:20:58 +0530

    [Added]fin the minimum and maximum of the List

---
+++ DataStructure/Sets/MinMax.py
@@ -0,0 +1,5 @@
+# Write a Python program to find maximum and the minimum value in a set.
+set= {45,12,96,41,24,85}
+print(set) #printing set
+print("Maximum value of set : ",max(set))# max value
+print("Minimum value of set : ",min(set))# min value
