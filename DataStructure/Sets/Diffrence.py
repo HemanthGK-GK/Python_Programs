6c01278ea3b4bd789a61787a71295fcb47f26935
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-02 00:16:13 +0530

    [Added]Diffrence of two Sets

---
+++ DataStructure/Sets/Diffrence.py
@@ -0,0 +1,9 @@
+# Write a Python program to create set difference.
+set1 = set(["green", "blue","red"])
+set2 = set(["blue", "red","white"])
+print("Set elements :")
+print(set1)
+print(set2)
+print("\n Diffrence of two sets:")
+result = set1.difference(set2) #Diffrence of two sets
+print(result)
