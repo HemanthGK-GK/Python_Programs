051d1451b4ff08a4156fba55470ffe9ba3b2d06f
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-01 23:26:23 +0530

    [Added]remove the first occurrence of a specified element from an Array

---
+++ DataStructure/Array/occurrenceofElement.py
@@ -0,0 +1,4 @@
+#Write a Python program to get the number of occurrences of a specified element in an array.
+from array import *
+arr= array('i', [21,14,63,21,57,21])#declaring array
+print("Number of occurrences : "+str(arr.count(21)))
