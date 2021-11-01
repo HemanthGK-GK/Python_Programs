9925d7bf24f2a0a569474c8b6af0e07e4901e0ca
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-02 00:25:24 +0530

    [Added]Creating tuple and some basic Methods

---
+++ DataStructure/Tuple/CreateTuple.py
@@ -0,0 +1,12 @@
+# Write a Python program to create a tuple with different data types.
+tuple=(10,20,30,40,50)
+print(tuple)#creating tuple of int type
+
+a,b,c,d,e=tuple
+print(a,b,c,d,e)#unpack a tuple
+
+tuple = ("MyData", False, 10.5, 71)
+print(tuple)#creating tuple of diffrent data type
+
+
+
