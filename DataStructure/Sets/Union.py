44aa2dc2902a8294ed4e7cbc18229a4055cd0727
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-02 00:23:35 +0530

    [Added]gert the union of two sets

---
+++ DataStructure/Sets/Union.py
@@ -0,0 +1,9 @@
+# Write a Python program to create a union of sets.
+set1 = set(["green", "blue","red"])
+set2 = set(["blue", "red","white"])
+print("Set elements :")
+print(set1)
+print(set2)
+print("\n Union of two sets:")
+result = set1.union(set2) #Union of two sets
+print(result)
