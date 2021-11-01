6dcbf1f8749af741b78317aa38b3c9dce26a9c2b
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-02 00:18:51 +0530

    [Added]Get the intersection of the two sets

---
+++ DataStructure/Sets/Intersection.py
@@ -0,0 +1,9 @@
+# Write a Python program to create an intersection of sets.
+set1 = set(["green", "blue","red"])
+set2 = set(["blue", "red","white"])
+print("Set elements :")
+print(set1)
+print(set2)
+print("\nIntersection of two sets:")
+result = set1 & set2 #intersection of two sets
+print(result)
