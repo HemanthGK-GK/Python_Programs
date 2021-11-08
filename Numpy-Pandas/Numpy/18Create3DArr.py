8684e24e547ae769f0b0e2989e3d61e4b612109a
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-08 21:53:40 +0530

    [Added]Create and display 3 dimension array

---
+++ Numpy-Pandas/Numpy/18Create3DArr.py
@@ -0,0 +1,9 @@
+# Write a Python program to create a 3-D array with ones on a diagonal and zeros
+# elsewhere.
+# Expected Output:
+# [[ 1. 0. 0.]
+# [ 0. 1. 0.]
+# [ 0. 0. 1.]]
+import numpy as np
+x = np.eye(3)
+print(x)
