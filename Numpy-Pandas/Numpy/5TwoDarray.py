3634d02642db0a9725b60491998fb21a0d807026
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-08 21:43:55 +0530

    [Added]creating two dimension array

---
+++ Numpy-Pandas/Numpy/5TwoDarray.py
@@ -0,0 +1,15 @@
+# Write a Python program to create a 2d array with 1 on the border and 0 inside.
+# Expected Output:
+# Original array:
+# [[ 1. 1. 1. 1. 1.]
+# [ 1. 1. 1. 1. 1.]
+# [ 1. 1. 1. 1. 1.]
+# [ 1. 1. 1. 1. 1.]
+# [ 1. 1. 1. 1. 1.]]
+import numpy as np
+x = np.ones((5,5))
+print("Original array:")
+print(x)
+print("1 on the border and 0 inside in the array")
+x[1:-1,1:-1] = 0
+print(x)
