22386a5d1951d62455e4905655f197dcd3a9298e
HemanthGK-GK <hemanthgk804@gmail.com>
2021-11-08 21:58:27 +0530

    [Added]create and display the ! dimensional array

---
+++ Numpy-Pandas/Pandas/1Create1DArray.py
@@ -0,0 +1,5 @@
+# Write a Python program to create and display a one-dimensional array-like object
+# containing an array of data using Pandas module
+import pandas as pd
+ds = pd.Series([2, 4, 6, 8, 10])
+print(ds)
